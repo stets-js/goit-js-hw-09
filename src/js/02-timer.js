0d72e89bb588c84f8234c121f3af727ca5db9073
Dmytro Stetsenko <d.a.stetsenko@gmail.com>
2022-08-15 23:03:53 +0300

    task 2

---
+++ src/js/02-timer.js
@@ -0,0 +1,40 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const startBtn = document.querySelector('button[data-start]');
+
+startBtn.setAttribute('disabled', true);
+
+flatpickr('input#datetime-picker', {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < new Date()) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+    }
+    startBtn.removeAttribute('disabled');
+  },
+});
+
+// CONVERT
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
