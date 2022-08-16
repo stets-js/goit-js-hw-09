459d6cc7a6510e0694919f7b4902684c45ccfa14
Dmytro Stetsenko <d.a.stetsenko@gmail.com>
2022-08-16 17:59:50 +0300

    css styles task2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,5 @@
 import flatpickr from 'flatpickr';
-import 'flatpickr/dist/flatpickr.min.css';
+import 'flatpickr/dist/themes/material_blue.css';
 import Notiflix from 'notiflix';
 
 const input = document.querySelector('#datetime-picker');
