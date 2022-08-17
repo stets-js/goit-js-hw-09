198740b905b2ec39ff5579b1fc95bfe31eb5120a
stets-js <97848708+stets-js@users.noreply.github.com>
2022-08-17 17:40:30 +0000

    Deploying to gh-pages from @ stets-js/goit-js-hw-09@81493ad3ca88c0362fa62da640d333e4dc0a3ff7 🚀

---
+++ 01-color-switcher.afe68344.js
@@ -0,0 +1,2 @@
+!function(){const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");document.querySelector("head");function d(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,t.disabled=!0,e.disabled=!1}e.disabled=!0,t.addEventListener("click",d);let n=setInterval(d,1e3);e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.afe68344.js.map
